638eb07c3af8604b189419799d738851958fc899
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2023-11-08 16:31:24 +0100

    start project

--- finance_flow/src/App.js
+++ finance_flow/src/App.js
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+import  Header from './composant/Header';
 
 export default function MyApp() {
-  const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+
+
 
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <p>lololol suka blyat</p>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-
+    <div classname='header'>
+        <Header/>
     </div>
   );
 }
 
-function MyButton({ count, onClick }) {
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
-}
+
---
+++ finance_flow/src/composant/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            Finance Flow
+        </header>
+    )
+};
+
+export default Header;
